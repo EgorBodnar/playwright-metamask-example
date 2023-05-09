e81d9e509077f37a6016728344ba692410bfe902
EgorBodnar <maxameron@gmail.com>
2023-05-09 11:23:47 +0800

    chore: remove unused dependency on this stage

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["allure-playwright"], ["list", { printSteps: true }]],
+  reporter: [["html"], ["list", { printSteps: true }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.FRONTEND_URL,
